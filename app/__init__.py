cda5a5988cc2c00e162f30ad7f087d6839fbf633
Leandro <sistemas4adg@gmail.com>
2022-10-15 16:27:25 -0500

    primer commit

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        FROM_MAIL=os.environ.get('FROM_MAIL'),
+        MAIL_PASS=os.environ.get('MAIL_PASS'),
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+    )
+
+    from . import portfolio
+
+    app.register_blueprint(portfolio.bp)
+
+    return app
